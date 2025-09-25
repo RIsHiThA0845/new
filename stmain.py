8dce59dff39d34e57aa3bd07420eb9dbfdfa60e3
Rishitha Boddula <rishitha.boddula@accellor.com>
2025-09-25 09:59:46 +0530

    adding stremlit file

---
+++ stmain.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+BASE_URL=os.getenv("base_url")
+ 
+st.title("Item Manager")
+ 
+st.header("Add Item")
+name = st.text_input("Name")
+price = st.number_input("Price", min_value=0.0)
+ 
+if st.button("Save"):
+    data = {"name": name, "price": price}
+    res = requests.post(f"{BASE_URL}/item", json=data)
+    st.write(res.json())
+ 
+
+st.header("All Items")
+res = requests.get(f"{BASE_URL}/items")
+if res.status_code == 200:
+    st.json(res.json())
+ 
